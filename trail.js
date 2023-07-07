691e052b21b050ecd6aaa995740844344c3b92f0
CHAYAN PAL <120254873+palC02@users.noreply.github.com>
2023-07-07 16:11:49 +0530

    Add files via upload
    First Day of this course Internet of Technology, I try to make a website using html, css and javascript

---
+++ trail.js
@@ -0,0 +1,8 @@
+function message(){
+    alert("You Now Move to Your Next Step")
+    var a=parseInt(prompt("Enter the Value"))
+    var b=parseInt(prompt("Enter the value"))
+    var c=(a+b)
+    alert("Now you can check your result")
+    document.write(c)
+}
