63101e002ddd449963665506e853658c88c308aa
CHAYAN PAL <120254873+palC02@users.noreply.github.com>
2023-08-17 09:21:42 +0530

    Create C.java

---
+++ Jar_Code/Barun/C.java
@@ -0,0 +1,14 @@
+package Barun;
+
+public class C {
+    static int a;
+    public static void main(String[] args) {
+        a = 111;
+        System.out.println("Inside A Class");
+    }
+    public static void show(){
+        System.out.println("Value of a is = " + a);
+    }
+} 
+    
+
